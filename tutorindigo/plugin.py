0cd3f562aa03e1c5709561ea97527bafcfeae012
hendi <hendinof22@gmail.com>
2023-08-27 21:13:31 +0700

    update to xulu related brandmark 6

--- tutorindigo/plugin.py
+++ tutorindigo/plugin.py
@@ -44,8 +44,10 @@ hooks.Filters.ENV_TEMPLATE_TARGETS.add_items(
 
 # Force the rendering of scss files, even though they are included in a "partials" directory
 hooks.Filters.ENV_PATTERNS_INCLUDE.add_items(
-    r"xulu-indigo/lms/static/sass/partials/lms/theme/",
-    r"xulu-indigo/cms/static/sass/partials/cms/theme/"
+    [
+        r"xulu-indigo/lms/static/sass/partials/lms/theme/",
+        r"xulu-indigo/cms/static/sass/partials/cms/theme/"
+    ]
 )
 
 # Load all configuration entries
